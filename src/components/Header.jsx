17950f013f9a87423a0a1c71b890d6342833cd0a
JonasG4 <garciajonas899@gmail.com>
2022-02-19 12:34:57 -0600

    Configurando entorno de desarrollo

---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import { AccessTime, Search } from '@material-ui/icons'
+function Header() {
+    return (
+        <HeaderContainer>
+            {/* Left */}
+            <HeaderLeft>
+                <HeaderAvatar
+                //Add onclick evt
+                />
+                <AccessTime />
+            </HeaderLeft>
+            {/* Search */}
+            <HeaderSearch>
+                <Search/>
+                <input placeholder="Buscar un mensaje" />
+            </HeaderSearch>
+
+            {/* Right */}
+            <HeaderRight>
+
+            </HeaderRight>
+        </HeaderContainer>
+    )
+}
+
+
+
+export default Header;
+
+// Styled components
+
+const HeaderContainer = styled.div`
+    display:flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
+`
+
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    
+    > .MuiSvgIcon-root{
+        margin-left: auto;
+        margin-right: 30px;
+    }
+`
+
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover{
+        opacity: .8;
+    }
+
+`
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius:6px;
+    background-color:#421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid solid;
+
+    >input{
+        background-color:transparent;
+        border:none;
+        text-align:center;
+        min-width: 30vw;
+        outline: none;
+        color: white;
+    }
+`
+
+const HeaderRight = styled.div`
+display: flex;
+
+`
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDo41T12Md_y9Q7LtIsoPIqwLJFMn902hY",
+    authDomain: "slack-clone-3fb0b.firebaseapp.com",
+    projectId: "slack-clone-3fb0b",
+    storageBucket: "slack-clone-3fb0b.appspot.com",
+    messagingSenderId: "462588964812",
+    appId: "1:462588964812:web:f0f03975d567f3ec0e3769"
+  };
